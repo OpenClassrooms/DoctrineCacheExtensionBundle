10c484667d0ffa2ab2ae84ec04bfde34ea72fe2f
Christelle <christelle.canovas@openclassrooms.com>
2022-03-03 17:51:53 +0100

    chore(sf-deprecations): deprecated TreeBuilder constructor and root method adjusted

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,8 +15,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('doctrine_cache_extension');
+        $treeBuilder = new TreeBuilder('doctrine_cache_extension');
+        $rootNode = $treeBuilder->getRootNode();
         $rootNode
             ->children()
             ->scalarNode('default_lifetime')->defaultValue(0)->end()
